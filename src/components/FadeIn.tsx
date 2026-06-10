fb0473a53e31a1a0977cea941b1da27281fdaebe
Helaina Ferraioli <helainaferraioli@Helainas-MacBook-Pro.local>
2026-06-10 19:35:49 -0400

    fix FadeIn visibility

--- src/components/FadeIn.tsx
+++ src/components/FadeIn.tsx
@@ -12,7 +12,7 @@ export default function FadeIn({
   className?: string;
 }) {
   const ref = useRef<HTMLDivElement>(null);
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(true);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
